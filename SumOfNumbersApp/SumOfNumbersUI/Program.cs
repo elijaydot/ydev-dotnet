252fc28d4832835d89896acf58b3614b3d04434e
Pranadot <pranadot@gmail.com>
2021-05-05 22:02:23 +0100

    Sum Of Numbers app

---
+++ SumOfNumbersApp/SumOfNumbersUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfNumbersUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = SumOfNumbers(number);
+            Console.WriteLine($"The Sum of Numbers is : {sum}");
+            Console.ReadKey();
+        }
+        static int SumOfNumbers(int number)
+        {
+            if (number != 0)
+            {
+                return (number % 10 + SumOfNumbers(number / 10));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
